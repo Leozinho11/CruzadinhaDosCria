b2185a039086d5e5786146ea06be2b85de650ba0
joobrandao <joojuaum@gmail.com>
2023-05-29 20:38:38 -0300

    Alterando js

--- Cruzadinha.js
+++ Cruzadinha.js
@@ -18,7 +18,9 @@ var threads = inputPergunta17.value;
 var cache = inputPergunta18.value;
 
 if (cpu != "CPU") {
-    var palavra = document.getElementById("inputPergunta1");
+    var letra1_1 = document.getElementById("letra1_1");
+    var letra2_1 = document.getElementById("letra2_1");
+    var letra3_1 = document.getElementById("letra3_1");
     palavra.classList.add("inputError")
 } else {
     palavra.classList.remove("inputError")
@@ -28,7 +30,10 @@ if (cpu != "CPU") {
 }
 
 if (ula != "ULA") {
-    var palavra = document.getElementById("inputPergunta2");
+    var letra1_2 = document.getElementById("letra1_2");
+    var letra2_2 = document.getElementById("letra2_2");
+    var letra3_2 = document.getElementById("letra3_2");
+
     palavra.classList.add("inputError")
 } else {
     palavra.classList.remove("inputError")
@@ -38,7 +43,20 @@ if (ula != "ULA") {
 }
 
 if (registradores != "REGISTRADORES") {
-    var palavra = document.getElementById("inputPergunta3");
+    var letra1_3 = document.getElementById("letra1_3");
+    var letra2_3 = document.getElementById("letra2_3");
+    var letra3_3 = document.getElementById("letra3_3");
+    var letra4_3 = document.getElementById("letra4_3");
+    var letra5_3 = document.getElementById("letra5_3");
+    var letra6_3 = document.getElementById("letra6_3");
+    var letra7_3 = document.getElementById("letra7_3");
+    var letra8_3 = document.getElementById("letra8_3");
+    var letra9_3 = document.getElementById("letra9_3");
+    var letra10_3 = document.getElementById("letra10_3");
+    var letra11_3 = document.getElementById("letra11_3");
+    var letra12_3 = document.getElementById("letra12_3");
+    var letra13_3 = document.getElementById("letra13_3");
+
     palavra.classList.add("inputError")
 } else {
     palavra.classList.remove("inputError")
@@ -58,7 +76,10 @@ if (registradores != "REGISTRADORES") {
 }
 
 if (ram != "RAM") {
-    var palavra = document.getElementById("inputPergunta4");
+    var letra1_4 = document.getElementById("letra1_4");
+    var letra2_4 = document.getElementById("letra2_4");
+    var letra3_4 = document.getElementById("letra3_4");
+
     palavra.classList.add("inputError")
 } else {
     palavra.classList.remove("inputError")
@@ -68,7 +89,10 @@ if (ram != "RAM") {
 }
 
 if (rom != "ROM") {
-    var palavra = document.getElementById("inputPergunta5");
+    var letra1_5 = document.getElementById("letra1_5");
+    var letra2_5 = document.getElementById("letra2_5");
+    var letra3_5 = document.getElementById("letra3_5");
+
     palavra.classList.add("inputError")
 } else {
     palavra.classList.remove("inputError")
@@ -78,7 +102,12 @@ if (rom != "ROM") {
 }
 
 if (eprom != "EPROM") {
-    var palavra = document.getElementById("inputPergunta6");
+    var letra1_6 = document.getElementById("letra1_6");
+    var letra2_6 = document.getElementById("letra2_6");
+    var letra3_6 = document.getElementById("letra3_6");
+    var letra4_6 = document.getElementById("letra4_6");
+    var letra5_6 = document.getElementById("letra5_6");
+
     palavra.classList.add("inputError")
 } else {
     palavra.classList.remove("inputError")
@@ -90,7 +119,12 @@ if (eprom != "EPROM") {
 }
 
 if (flash != "FLASH") {
-    var palavra = document.getElementById("inputPergunta7");
+    var letra1_7 = document.getElementById("letra1_7");
+    var letra2_7 = document.getElementById("letra2_7");
+    var letra3_7 = document.getElementById("letra3_7");
+    var letra4_7 = document.getElementById("letra4_7");
+    var letra5_7 = document.getElementById("letra5_7");
+
     palavra.classList.add("inputError")
 } else {
     palavra.classList.remove("inputError")
@@ -101,8 +135,22 @@ if (flash != "FLASH") {
     letra5_7.innerHTML = resposta7[4]
 }
 
-if (memoriaMassa != "MEMÓRIA DE MASSA") {
-    var palavra = document.getElementById("inputPergunta8");
+if (memoriaMassa != "MEMÓRIA DE MASSA") { //conta os espaços?
+    var letra1_8 = document.getElementById("letra1_8");
+    var letra2_8 = document.getElementById("letra2_8");
+    var letra3_8 = document.getElementById("letra3_8");
+    var letra4_8 = document.getElementById("letra4_8");
+    var letra5_8 = document.getElementById("letra5_8");
+    var letra6_8 = document.getElementById("letra6_8");
+    var letra7_8 = document.getElementById("letra7_8");
+    var letra8_8 = document.getElementById("letra8_8");
+    var letra9_8 = document.getElementById("letra9_8");
+    var letra10_8 = document.getElementById("letra10_8");
+    var letra11_8 = document.getElementById("letra11_8");
+    var letra12_8 = document.getElementById("letra12_8");
+    var letra13_8 = document.getElementById("letra13_8");
+    var letra14_8 = document.getElementById("letra14_8");
+
     palavra.classList.add("inputError")
 } else {
     palavra.classList.remove("inputError")
@@ -125,7 +173,10 @@ if (memoriaMassa != "MEMÓRIA DE MASSA") {
 }
 
 if (dma != "DMA") {
-    var palavra = document.getElementById("inputPergunta9");
+    var letra1_9 = document.getElementById("letra1_9");
+    var letra2_9 = document.getElementById("letra2_9");
+    var letra3_9 = document.getElementById("letra3_9");
+
     palavra.classList.add("inputError")
 } else {
     palavra.classList.remove("inputError")
@@ -135,7 +186,9 @@ if (dma != "DMA") {
 }
 
 if (cs != "CS") {
-    var palavra = document.getElementById("inputPergunta10");
+    var letra1_10 = document.getElementById("letra1_10");
+    var letra2_10 = document.getElementById("letra2_10");
+
     palavra.classList.add("inputError")
 } else {
     palavra.classList.remove("inputError")
@@ -143,8 +196,18 @@ if (cs != "CS") {
     letra2_10.innerHTML = resposta10[1]
 }
 
-if (addressBus != "ADDRESS BUS") {
-    var palavra = document.getElementById("inputPergunta11");
+if (addressBus != "ADDRESS BUS") { //conta espaço?
+    var letra1_11 = document.getElementById("letra1_11");
+    var letra2_11 = document.getElementById("letra2_11");
+    var letra3_11 = document.getElementById("letra3_11");
+    var letra4_11 = document.getElementById("letra4_11");
+    var letra5_11 = document.getElementById("letra5_11");
+    var letra6_11 = document.getElementById("letra6_11");
+    var letra7_11 = document.getElementById("letra7_11");
+    var letra8_11 = document.getElementById("letra8_11");
+    var letra9_11 = document.getElementById("letra9_11");
+    var letra10_11 = document.getElementById("letra10_11");
+
     palavra.classList.add("inputError")
 } else {
     palavra.classList.remove("inputError")
@@ -161,8 +224,14 @@ if (addressBus != "ADDRESS BUS") {
     letra11_11.innerHTML = resposta11[9]
 }
 
-if (dataBus != "DATA BUS") {
-    var palavra = document.getElementById("inputPergunta12");
+if (dataBus != "DATA BUS") { //conta espaço?
+    var letra1_12 = document.getElementById("letra1_12");
+    var letra2_12 = document.getElementById("letra2_12");
+    var letra3_12 = document.getElementById("letra3_12");
+    var letra4_12 = document.getElementById("letra4_12");
+    var letra5_12 = document.getElementById("letra5_12");
+    var letra6_12 = document.getElementById("letra6_12");
+    var letra7_12 = document.getElementById("letra7_12");
     palavra.classList.add("inputError")
 } else {
     palavra.classList.remove("inputError")
@@ -177,7 +246,9 @@ if (dataBus != "DATA BUS") {
 }
 
 if (i5 != "I5") {
-    var palavra = document.getElementById("inputPergunta13");
+    var letra1_13 = document.getElementById("letra1_13");
+    var letra2_13 = document.getElementById("letra2_13");
+
     palavra.classList.add("inputError")
 } else {
     palavra.classList.remove("inputError")
@@ -186,7 +257,9 @@ if (i5 != "I5") {
 }
 
 if (i7 != "I7") {
-    var palavra = document.getElementById("inputPergunta14");
+    var letra1_14 = document.getElementById("letra1_14");
+    var letra2_14 = document.getElementById("letra2_14");
+
     palavra.classList.add("inputError")
 } else {
     palavra.classList.remove("inputError")
@@ -194,8 +267,16 @@ if (i7 != "I7") {
     letra2_14.innerHTML = resposta14[1]
 }
 
-if (dualCore != "DUAL CORE") {
-    var palavra = document.getElementById("inputPergunta15");
+if (dualCore != "DUAL CORE") { //conta espaço?
+    var letra1_15 = document.getElementById("letra1_15");
+    var letra2_15 = document.getElementById("letra2_15");
+    var letra3_15 = document.getElementById("letra3_15");
+    var letra4_15 = document.getElementById("letra4_15");
+    var letra5_15 = document.getElementById("letra5_15");
+    var letra6_15 = document.getElementById("letra6_15");
+    var letra7_15 = document.getElementById("letra7_15");
+    var letra8_15 = document.getElementById("letra8_15");
+
     palavra.classList.add("inputError")
 } else {
     palavra.classList.remove("inputError")
@@ -211,7 +292,15 @@ if (dualCore != "DUAL CORE") {
 }
 
 if (quadCore != "QUAD CORE") {
-    var palavra = document.getElementById("inputPergunta16");
+    var letra1_16 = document.getElementById("letra1_16");
+    var letra2_16 = document.getElementById("letra2_16");
+    var letra3_16 = document.getElementById("letra3_16");
+    var letra4_16 = document.getElementById("letra4_16");
+    var letra5_16 = document.getElementById("letra5_16");
+    var letra6_16 = document.getElementById("letra6_16");
+    var letra7_16 = document.getElementById("letra7_16");
+    var letra8_16 = document.getElementById("letra8_16");
+
     palavra.classList.add("inputError")
 } else {
     palavra.classList.remove("inputError")
@@ -227,7 +316,14 @@ if (quadCore != "QUAD CORE") {
 }
 
 if (threads != "THREADS") {
-    var palavra = document.getElementById("inputPergunta17");
+    var letra1_17 = document.getElementById("letra1_17");
+    var letra2_17 = document.getElementById("letra2_17");
+    var letra3_17 = document.getElementById("letra3_17");
+    var letra4_17 = document.getElementById("letra4_17");
+    var letra5_17 = document.getElementById("letra5_17");
+    var letra6_17 = document.getElementById("letra6_17");
+    var letra7_17 = document.getElementById("letra7_17");
+    
     palavra.classList.add("inputError")
 } else {
     palavra.classList.remove("inputError")
@@ -241,7 +337,12 @@ if (threads != "THREADS") {
 }
 
 if (cache != "CACHE") {
-    var palavra = document.getElementById("inputPergunta18");
+    var letra1_18 = document.getElementById("letra1_18");
+    var letra2_18 = document.getElementById("letra2_18");
+    var letra3_18 = document.getElementById("letra3_18");
+    var letra4_18 = document.getElementById("letra4_18");
+    var letra5_18 = document.getElementById("letra5_18");
+    
     palavra.classList.add("inputError")
 } else {
     palavra.classList.remove("inputError")
